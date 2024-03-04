847e485613bfe72b59b5fb22a78ae9e0b3547e9d
yaoleksa <mryaremchyk@gmail.com>
2024-03-04 11:58:28 +0200

    ChatGPT helps me

--- recover/recover.c
+++ recover/recover.c
@@ -1,64 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-	// check command line arguments quantity
-	if(argc != 2)
-	{
-		fprintf(stderr, "Usage: recover.exe filename\n");
-		return 1;
-	}
-	
-	// open file
-	FILE *file = fopen(argv[1], "rb");
-	
-	if(file == NULL)
-	{
-		fprintf(stderr, "Could not open file\n");
-		return 2;
-	}
-	
-	// read file by bytes
-	uint8_t ch[3];
-	int eof;
-	int i = 0;
-	FILE *outfile;
-	while(eof != EOF)
-	{
-		fread(&ch, sizeof(ch), 1, file);
-		eof = getc(file);
-		char buffer[19];
-		if(ch[0] == 255 && ch[1] == 216 && ch[2] == 255)
-		{
-			if(i < 10)
-			{
-				sprintf(buffer, "00%d.jpeg", i);
-			}
-			else if(i < 100)
-			{
-				sprintf(buffer, "0%d.jpeg", i);
-			}
-			else
-			{
-				sprintf(buffer, "%d.jpeg", i);
-			}
-			if(i > 0)
-			{
-				// Close this file
-				fclose(outfile);
-			}
-			outfile = fopen(buffer, "w");
-			i++;
-			// Write to outfile
-			fwrite(&ch, sizeof(ch), 1, outfile);
-		}
-		// Continue to write
-		//fwrite(&ch, sizeof(uint8_t), 1, outfile);
-	}
-	// Close .raw file
-	fclose(file);
-	fclose(outfile);
-	return 0;
+    // check command line arguments quantity
+    if(argc != 2)
+    {
+        fprintf(stderr, "Usage: recover.exe filename\n");
+        return 1;
+    }
+    
+    // open file
+    FILE *file = fopen(argv[1], "rb");
+    
+    if(file == NULL)
+    {
+        fprintf(stderr, "Could not open file\n");
+        return 2;
+    }
+    
+    // read file by bytes
+    uint8_t ch[3];
+    int eof;
+    int i = 0;
+    FILE *outfile = NULL; // Initialize to NULL
+    int file_opened = 0; // Flag to check if outfile is opened
+    while(eof != EOF)
+    {
+        fread(&ch, sizeof(ch), 1, file);
+        eof = getc(file);
+        char buffer[19];
+        if(ch[0] == 255 && ch[1] == 216 && ch[2] == 255)
+        {
+            if(i < 10)
+            {
+                sprintf(buffer, "00%d.jpeg", i);
+            }
+            else if(i < 100)
+            {
+                sprintf(buffer, "0%d.jpeg", i);
+            }
+            else
+            {
+                sprintf(buffer, "%d.jpeg", i);
+            }
+            if(file_opened) // If outfile is already opened, close it
+            {
+                fclose(outfile);
+            }
+            outfile = fopen(buffer, "w");
+            if(outfile == NULL)
+            {
+                fprintf(stderr, "Could not create file %s\n", buffer);
+                fclose(file);
+                return 3;
+            }
+            ftruncate(fileno(outfile), 512);
+            fseek(outfile, 0, SEEK_END);
+            printf("size %ld\n", ftell(outfile));
+            //printf("size is %ld\n", size);
+            file_opened = 1; // Set flag to indicate outfile is opened
+            i++;
+        }
+        if(file_opened)
+        {
+            fwrite(&ch, sizeof(ch), 1, outfile);
+        }
+    }
+    // Close .raw file
+    fclose(file);
+    if(file_opened) // If outfile is opened, close it
+    {
+        fclose(outfile);
+    }
+    return 0;
 }
