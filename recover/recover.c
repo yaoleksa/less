cc9cb6157b0003f705e4513b3f1d160df49ca398
yaoleksa <mryaremchyk@gmail.com>
2024-03-05 10:23:59 +0200

    ChatGPT is genius!

--- recover/recover.c
+++ recover/recover.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     // check command line arguments quantity
-    if(argc != 2)
-    {
+    if(argc != 2) {
         fprintf(stderr, "Usage: recover.exe filename\n");
         return 1;
     }
@@ -15,62 +12,40 @@ int main(int argc, char *argv[])
     // open file
     FILE *file = fopen(argv[1], "rb");
     
-    if(file == NULL)
-    {
+    if(file == NULL) {
         fprintf(stderr, "Could not open file\n");
         return 2;
     }
     
     // read file by bytes
-    uint8_t ch[3];
-    int eof;
+    uint8_t ch[512]; // 512 bytes buffer
     int i = 0;
     FILE *outfile = NULL; // Initialize to NULL
-    int file_opened = 0; // Flag to check if outfile is opened
-    while(eof != EOF)
-    {
-        fread(&ch, sizeof(ch), 1, file);
-        eof = getc(file);
-        char buffer[19];
-        if(ch[0] == 255 && ch[1] == 216 && ch[2] == 255)
-        {
-            if(i < 10)
-            {
-                sprintf(buffer, "00%d.jpeg", i);
-            }
-            else if(i < 100)
-            {
-                sprintf(buffer, "0%d.jpeg", i);
-            }
-            else
-            {
-                sprintf(buffer, "%d.jpeg", i);
-            }
-            if(file_opened) // If outfile is already opened, close it
-            {
+    while (fread(&ch, sizeof(ch), 1, file) == 1) {
+        char buffer[8];
+        if (ch[0] == 0xff && ch[1] == 0xd8 && ch[2] == 0xff && (ch[3] & 0xf0) == 0xe0) {
+            if (outfile != NULL) {
                 fclose(outfile);
             }
+            sprintf(buffer, "%03d.jpg", i);
             outfile = fopen(buffer, "wb");
-            if(outfile == NULL)
-            {
+            if (outfile == NULL) {
                 fprintf(stderr, "Could not create file %s\n", buffer);
                 fclose(file);
                 return 3;
             }
             fwrite(&ch, sizeof(ch), 1, outfile);
-            file_opened = 1; // Set flag to indicate outfile is opened
             i++;
-        }
-        else if(file_opened)
-        {
+        } else if (outfile != NULL) {
             fwrite(&ch, sizeof(ch), 1, outfile);
         }
     }
-    // Close .raw file
+    
+    // Close files
     fclose(file);
-    if(file_opened) // If outfile is opened, close it
-    {
+    if(outfile != NULL) {
         fclose(outfile);
     }
+    
     return 0;
 }
